950cb2cd963917cd3c3eda34f3d4087a12ba3720
webdevforfoodz <73584854+webdevforfoodz@users.noreply.github.com>
2020-12-13 21:48:32 -0800

    updating second section and adding third section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import FirstSection from './sections/FirstSection'
+import FirstSection from './sections/FirstSection';
 import SecondSection from './sections/SecondSection';
+import ThirdSection from './sections/ThirdSection';
 
 function App() {
   return (
     <div className="App">
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
     </div>
   );
 }
---
+++ src/sections/ThirdSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/third-section.css';
+
+function ThirdSection() {
+    return (
+        <div className="third-section" >
+            <div className="third-section-title">
+                <span>Governance-as-a-Service (GaaS)</span>
+                <span>We are a role model in community governance done right. </span>
+            </div>
+        </div>
+    )
+}
+
+export default ThirdSection;
