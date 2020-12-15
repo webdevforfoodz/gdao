57796d217e598b1776b8b85aef60964e4241b962
webdevforfoodz <73584854+webdevforfoodz@users.noreply.github.com>
2020-12-15 02:29:12 -0800

    third-section

--- src/components/ThirdSectionCard.js
+++ src/components/ThirdSectionCard.js
@@ -3,9 +3,18 @@ import React from 'react'
 function ThirdSectionCard(props) {
     return (
         <div className="third-section-card" >
+            <div className="third-section-card-img-wrapper">
+                <img src={props.img} alt={props.img}/>
+                <div className="third-section-card-img-wrapper-content">
+                    <span>{props.title}</span>
+                    <span>{props.content}
+                    </span>
+                </div>
+            </div>
             <div className="third-section-card-title">
                 {props.title}
             </div>
+            <button>Learn more</button>
         </div>
     )
 }
--- src/sections/ThirdSection.js
+++ src/sections/ThirdSection.js
@@ -10,7 +10,13 @@ function ThirdSection() {
                 <span>We are a role model in community governance done right. </span>
             </div>
             <div className="third-section-cards-wrapper">
-                <Card />
+                <Card title={"Pre-deployment Consulting"} img={"/images/consulting.svg"} content={"Utilization of Governor resources and smart contracts to best equip third-party projects to launch with a sound framework in place."}/>
+                <Card title={"Voting Bootstrap"} img={"/images/vote.svg"} content={"The mature, engaged Governor community participate as active voters on third-party projects. This ensures that voting quorums are met and long-term interests are upheld from launch onwards."}/>
+                <Card title={"Unrug-as-a-Service"} img={"/images/lab.svg"} content={"Governor rose from the community of the biggest exitscam of the year. Other communities that were rugged leverage our framework to rebuild their own project for themselves."}/>
+            </div>
+            <div className="third-section-footer" >
+                Our protocol offers several services as part of the Governance-as-a-Service 
+                (GaaS) sandbox for any project to ensure their governance model works out of the box.
             </div>
         </div>
     )
